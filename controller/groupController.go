36a86c8097ed4132151b624372ada941b364a23f
yebology <139052533+yebology@users.noreply.github.com>
2024-12-31 20:27:34 +0700

    add validation on group struct and change id to user_id on get group logic

--- controller/groupController.go
+++ controller/groupController.go
@@ -18,7 +18,7 @@ func GetGroup(c *fiber.Ctx) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	id := c.Params("id")
+	id := c.Params("user_id")
 	objectId, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
 		return output.GetError(c, fiber.StatusBadRequest, string(constant.InvalidIdError))
--- model/group.go
+++ model/group.go
@@ -5,10 +5,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Group struct {
 
 	Id 					primitive.ObjectID 		`json:"id" bson:"_id,omitempty"`
-	GroupOwnerId		primitive.ObjectID		`json:"groupOwnerId" bson:"_groupOwnerId"`
-	GroupMemberIds 		[]primitive.ObjectID 	`json:"groupMemberIds" bson:"_groupMemberIds,omitempty"`
-	GroupName 			string 					`json:"groupName"`
-	GroupImageHash 		string 					`json:"groupImageHash"`
-	GroupDescription 	string 					`json:"groupDescription"`
+	GroupOwnerId		primitive.ObjectID		`json:"groupOwnerId" bson:"_groupOwnerId" validate:"required"`
+	GroupMemberIds 		[]primitive.ObjectID 	`json:"groupMemberIds" bson:"_groupMemberIds"`
+	GroupName 			string 					`json:"groupName" validate:"required,min=8,max=100"`
+	GroupImageHash 		string 					`json:"groupImageHash" validate:"required"`
+	GroupDescription 	string 					`json:"groupDescription" validate:"required,min=8,max=255"`
 
 }
