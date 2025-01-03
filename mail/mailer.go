cf82e6e42eb2f3953ee14b6e66b6e550ca168586
yebology <139052533+yebology@users.noreply.github.com>
2025-01-03 14:01:51 +0700

    still works on mailer

--- mail/mailer.go
+++ mail/mailer.go
@@ -1,7 +1,34 @@
 package mail
 
+import (
+	"html/template"
 
-func SendGreetingEmail(email string) error {
+	"gopkg.in/gomail.v2"
+)
+
+
+func SendGreetingEmail(email string, name string) error {
+
+	html, err := template.ParseFiles("../view/greeting.html")
+	if err != nil {
+		return err
+	}
+
+	err = html.Execute()
+
+	senderEmail := "yobelnathaniel12@gmail.com"
+	senderPassword := ""
+
+	mail := gomail.NewMessage()
+	mail.SetHeader("From", senderEmail)
+	mail.SetHeader("To", email)
+	mail.SetHeader("Subject", "Welcome to Giggle! Let’s Get Started 🚀")
+
+	dialer := gomail.NewDialer("smtp.gmail.com", 587, senderEmail, senderPassword)
+	err = dialer.DialAndSend(mail)
+	if err != nil {
+		return err
+	}
 
 	return nil
 
