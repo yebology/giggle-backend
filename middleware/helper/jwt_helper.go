7cb094de979b198166c82e1e18370ced5e2cce39
yebology <139052533+yebology@users.noreply.github.com>
2025-01-25 13:19:23 +0700

    update comment on jwt_helper.go

--- middleware/helper/jwt_helper.go
+++ middleware/helper/jwt_helper.go
@@ -4,23 +4,21 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 )
-
-// ParseToken is responsible for extracting and validating the JWT from the HTTP request.
-// It looks for the token in either the Authorization header or the query parameter.
+// ParseToken extracts the token from the request and parses it to validate its claims.
 func ParseToken(c *fiber.Ctx) (jwt.MapClaims, error) {
 
-	// Attempt to get the token from the "Authorization" header.
+	// Get the token from the Authorization header or query parameter
 	token := c.Get("Authorization")
 	if token == "" {
-		// If the token is not found in the header, try getting it from the query string.
 		token = c.Query("Authorization")
-	}
+	}	
 
-	// Initialize an empty map to store the claims extracted from the token.
+	// Define claims to hold the decoded data
 	claims := jwt.MapClaims{}
 	
-	// Parse the token and extract the claims, using the secret key from the GetSecretKey function.
 	// The token is validated using the secret key.
+	// jwt.ParseWithClaims parses the JWT token and validates its claims using the GetSecretKey function.
+	// This function will call GetSecretKey and pass the token as an argument.
 	parsedToken, err := jwt.ParseWithClaims(token, claims, GetSecretKey)
 	if err != nil || !parsedToken.Valid {
 		// If there’s an error parsing the token or it is invalid, return an error.
@@ -36,7 +34,10 @@ func ParseToken(c *fiber.Ctx) (jwt.MapClaims, error) {
 // In this case, it returns a hardcoded secret key. In production, consider storing this securely.
 func GetSecretKey(token *jwt.Token) (interface{}, error) {
 
-	// Return the secret key used for signing the JWT. It's critical that the same key is used to sign and verify the token.
+	// Return the secret key used for signing the JWT. 
+	// It's critical that the same key is used to sign and verify the token.
+	// While the token parameter is passed, it is not used directly in this function,
+	// but it's required by jwt.ParseWithClaims as a part of its validation process.
 	return []byte("secret-key"), nil
 
 }
